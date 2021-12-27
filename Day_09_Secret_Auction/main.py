41aaac661d8c6a1418b2b52ea3d76bb2b03dbd51
Felipe Rojas <67447488+FelipeZerokun@users.noreply.github.com>
2021-12-27 04:34:10 -0500

    Add files via upload

---
+++ Day_09_Secret_Auction/main.py
@@ -0,0 +1,42 @@
+import art
+import os
+clear = lambda: os.system('cls')
+
+#HINT: You can call clear() to clear the output in the console.
+    
+print(art.logo)
+print("Secret Auction program, by Felipe Rojas")
+
+print(art.logo)
+print("Secret Auction program, by Felipe Rojas")
+
+bidders = []
+auction_end = False
+
+def new_entry(name, bid):
+  new_bidder = {}
+  new_bidder["name"] = name
+  new_bidder["bid"] = bid
+  bidders.append(new_bidder)
+
+while not auction_end:
+  bidder_name = input("Enter your name: ")
+  bid_quantity = int(input("Enter your bid: $"))
+  new_entry(bidder_name, bid_quantity)
+  continue_auction = input("Is there another person? (Yes or No): ").lower()
+  
+  if continue_auction == "no":
+    auction_end = True
+
+  elif continue_auction == "yes":
+    clear()
+
+top_bid = 0
+top_bidder = ""
+for person in bidders:
+  if person["bid"] > top_bid:
+    top_bidder = person["name"]
+    top_bid = person["bid"]
+
+
+print(f"{top_bidder} wins the bid with ${top_bid}")
