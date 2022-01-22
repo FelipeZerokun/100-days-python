67db8ae3c241573a16b34f6276a0a4fe973fdfb9
Felipe Rojas <67447488+FelipeZerokun@users.noreply.github.com>
2022-01-22 17:49:33 -0500

    Add files via upload

---
+++ Day_37_Habit_Tracking_app/main.py
@@ -0,0 +1,74 @@
+import requests
+import os
+from datetime import datetime
+
+# Following the steps in https://pixe.la/
+USERNAME = 'feliperojaspython3'
+TOKEN = 'GHUGRjbujGJHhyuGUHBfd'
+GRAPH_ID = 'graph1'
+
+# First step. Create user name
+pixela_endpoint = 'https://pixe.la/v1/users'      # This one is to Create a user
+create_user_params = {
+    'token': TOKEN,
+    'username': USERNAME,
+    'agreeTermsOfService': 'yes',
+    'notMinor': 'yes'
+}
+# response = requests.post(url= pixela_endpoint, json=create_user_params)
+# print(response.text)
+# I comment out because my user is already created
+
+# Second step. Create a Graph definition
+graph_endpoint = f'{pixela_endpoint}/{USERNAME}/graphs'
+graph_config = {
+    'id': GRAPH_ID,
+    'name': 'cycling_graph',
+    'unit': 'Km',
+    'type': 'float',
+    'color': 'momiji'
+}
+
+headers = {
+    'X-USER-TOKEN': TOKEN
+    }
+
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+# Commented out after created the graph
+
+# Third step. Get the graph!
+pixel_creation_endpoint = f'{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}'
+
+today = datetime.now()
+today = today.strftime("%Y%m%d")
+print(today)
+pixel_config = {
+    'date': today,
+    'quantity': input("How many KM did you run today?: "),
+}
+
+# response = requests.post(url=pixel_creation_endpoint, json=pixel_config, headers=headers)
+#print(response.text)
+
+
+# ##### Update a graph configuration
+# update_graph_url = f'{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}'
+# update_config = {
+#     'name': "New Name",
+#     'unit': "calory",
+# }
+#
+# response = requests.put(url=update_graph_url, json= update_config, headers=headers)
+#
+# ##### Update pixel data
+# update_pixel_url = f'{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today}'
+# update_config = {
+#     'quantity': "12"
+# }
+#
+# response = requests.put(url=update_graph_url, json= update_config, headers=headers)
+#
+# ###### Delete a pixel in a graph
+# delete_graph_url = f'{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today}'
+# response = requests.delete(url=delete_graph_url, headers=headers)
