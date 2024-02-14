90f208492efe101527449a3d63af6e6bd7e1c216
FelipeZero <felipe.rojas@stud.th-deg.de>
2024-02-14 15:47:35 +0100

    Adding individual environments to each project

--- Day_01_Band_Name_Generator/main.py
+++ Day_01_Band_Name_Generator/main.py
@@ -1,11 +1,15 @@
-#1. Create a greeting for your program.
+# Day one project. Band name generator.
+# Understanding Python variables and input function.
+# input will get data from the user in the form of a string.
+# Strings can be added together to get a new string variable.
+
 print("Band name Generator, by Felipe Rojas")
-#2. Ask the user for the city that they grew up in.
+# Ask the user for the city that they grew up in.
 city = input("Enter the city where you grew up in: ")
-#3. Ask the user for the name of a pet.
+# Ask the user for the name of a pet.
 pet = input("enter the name of a pet: ")
-#4. Combine the name of their city and pet and show them their band name.
+# Combine the name of their city and pet and show them their band name.
 band_name = city + pet
 print(band_name)
-#5. Make sure the input cursor shows on a new line, see the example at:
+# Make sure the input cursor shows on a new line, see the example at:
 #   https://band-name-generator-end.appbrewery.repl.run/
